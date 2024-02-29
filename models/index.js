daf45d9574597dce37159b6b75ade97c7b4255a3
amaher <amaher112@su.edu>
2024-02-29 15:17:28 -0500

    added code for api routes

--- models/index.js
+++ models/index.js
@@ -1,39 +1,38 @@
-const User = require('./User');
-const Comment = require('./Comment');
-const Post = require('./Post');
+const User = require("./User");
+const Comment = require("./Comment");
+const Post = require("./Post");
 
 // user has many comments
 User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 // user has many posts
 User.hasMany(Post, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 // comments have one user
 Comment.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 // comments have one post
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id'
+  foreignKey: "post_id",
 });
+
 // post has many comments
 Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-  onDelete: 'CASCADE'
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
 });
+
 // post has one user
 Post.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
-
-
-
 
 module.exports = { User, Comment, Post };
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controllers");
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
