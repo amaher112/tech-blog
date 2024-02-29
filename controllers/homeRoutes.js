c951284024de0938430d9d513b7af44654fffcd3
amaher <amaher112@su.edu>
2024-02-28 21:11:51 -0500

    added api code

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { Comment, User, Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        // Get all projects and JOIN with user data
+        const postData = await Post.findAll({
+          include: [
+            {
+              model: Post,
+              attributes: ['title', 'username', 'contents', 'date_created'],
+            },
+          ],
+        });
+    
+        // Serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+    
+        // Pass serialized data and session flag into template
+        res.render('homepage', { 
+          posts, 
+          logged_in: req.session.logged_in 
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
